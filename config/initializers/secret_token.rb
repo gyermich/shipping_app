9b4edfd12510295b440555c98b2b78193ce42d50
Christina Thompson <c.thompsonms@gmail.com>
2014-02-26 10:47:37 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShippingApp::Application.config.secret_key_base = '627c189d326a156929b1096df7fe425d259e1803a1ac2675ed1b4a58c71348e63e488f84a9eb60af8efa247fdea0a658811cebe67b881f148f9dc92459157b92'
